fa0289465dba854faee21d6a53cbee1b1695918c
George Goldberg <george@grundleborg.com>
2015-07-17 17:24:02 +0100

    Start to implement TrainStatusMessages.

--- darwinpush/messages/TrainStatusMessage.py
+++ darwinpush/messages/TrainStatusMessage.py
@@ -1,6 +1,25 @@
 from darwinpush.messages.BaseMessage import BaseMessage
 
+class LateReason:
+
+    def __init__(self, raw):
+        self.raw = raw
+
+    @property
+    def tiploc(self):
+        return self.raw.tiploc
+
+    @property
+    def near(self):
+        return self.raw.near
+
+
 class TrainStatusMessage(BaseMessage):
-    pass
+    
+    @property
+    def late_reason(self):
+        return self.raw.LateReason
+
+
 
 
