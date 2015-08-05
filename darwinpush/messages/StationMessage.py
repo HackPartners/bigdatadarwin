44a49c7fe1447b0531c06e3a15184c9a3fda2896
George Goldberg <george@grundleborg.com>
2015-08-05 14:00:41 +0100

    Implement StationMessage class and basic test case.

--- darwinpush/messages/StationMessage.py
+++ darwinpush/messages/StationMessage.py
@@ -1,6 +1,95 @@
 from darwinpush.messages.BaseMessage import BaseMessage
 
-class StationMessage(BaseMessage):
+from darwinpush.xb.sm import MsgCategoryType, MsgSeverityType
+
+import enum
+
+class UnknownStationMessageCategoryError(Exception):
     pass
+
+
+class UnknownStationMessageSeverityError(Exception):
+    pass
+
+
+class StationMessageCategory(enum.Enum):
+    Train = 0
+    Station = 1
+    Connection = 2
+    System = 3
+    Misc = 4
+    PriorTrain = 5
+    PriorOther = 6
+
+    @staticmethod
+    def from_xb(x):
+        if x == MsgCategoryType.Train:
+            return StationMessageCategory.Train
+        if x == MsgCategoryType.Station:
+            return StationMessageCategory.Station
+        if x == MsgCategoryType.Connections:
+            return StationMessageCategory.Connection
+        if x == MsgCatgoryType.System:
+            return StationMessageCategory.System
+        if x == MsgCategoryType.Misc:
+            return StationMessageCategory.Misc
+        if x == MsgCategoryType.PriorTrains:
+            return StationMessageCategory.PriorTrain
+        if x == MsgCategoryType.PriorOther:
+            return StationMessageType.PriorOther
+
+        raise UnknownStationMessageCategoryError("Unknown Station Message Category '{}' received in Station Message.".format(x))
+
+
+class StationMessageSeverity(enum.Enum):
+    LevelZero = 0
+    LevelOne = 1
+    LevelTwo = 2
+    LevelThree = 3
+
+    @staticmethod
+    def from_xb(x):
+        if x == MsgSeverityType.n0:
+            return StationMessageSeverity.LevelZero
+        if x == MsgSeverityType.n1:
+            return StationMessageSeverity.LevelOne
+        if x == MsgSeverityType.n2:
+            return StationMessageSeverity.LevelTwo
+        if x == MsgSeverityType.n3:
+            return StationMessageSeverity.LevelThree
+
+        raise UnknownStationMessageSeverityError("Unknown Station Message Severity '{}' received in Station Message.".format(x))
+
+
+class StationMessage(BaseMessage):
+
+    def __init__(self, message, containing_message):
+        super().__init__(message, containing_message)
+        self._build_stations_list()
+
+    def _build_stations_list(self):
+        self._stations = []
+        for s in self.raw.Station:
+            self._stations.append(s.crs)
+
+    @property
+    def stations(self):
+        return self._stations
+
+    @property
+    def message(self):
+        return self.raw.Msg.orderedContent()[0].value
+
+    @property
+    def smid(self):
+        return self.raw.id
+
+    @property
+    def category(self):
+        return StationMessageCategory.from_xb(self.raw.cat)
+
+    @property
+    def severity(self):
+        return StationMessageSeverity.from_xb(self.raw.sev)
 
 
--- tests/test_station_message.py
+++ tests/test_station_message.py
@@ -4,14 +4,31 @@ sys.path.append(os.getcwd())
 
 import pytest
 
-import darwinpush.xb.sm as sm
+import darwinpush.xb.pushport as pp
+from darwinpush.messages import StationMessage
+from darwinpush.messages.StationMessage import StationMessageCategory, StationMessageSeverity
 
 class TestStationMessage:
-    def test_station_message(self):
-        with open("tests/data/station_message.xml") as f:
-            #r = sm.CreateFromDocument(f.read())
-            #m = StationMessage(r)
 
-            assert(True)
+    def get_station_message_from_file(self, file_path):
+        with open(file_path) as f:
+            r = pp.CreateFromDocument(f.read())
+            assert(r.sR is None)
+            assert(r.uR is not None)
+            assert(len(r.uR.OW) == 1)
+
+            s = StationMessage(r.uR.OW[0], r)
+            return s
+
+    def test_station_message(self):
+        m = self.get_station_message_from_file("tests/data/station_message.xml")
+        assert(None is not m)
+
+        assert(1 == len(m.stations))
+        assert("BSK" == m.stations[0])
+        assert("The lifts on platforms 2 and 3 at Basingstoke will be out of use until at least September 2015. " == m.message)
+        assert(51504 == m.smid)
+        assert(StationMessageCategory.Station == m.category)
+        assert(StationMessageSeverity.LevelOne == m.severity)
 
 
