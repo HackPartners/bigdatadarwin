c60b5e3e4f5574bdc229a0c5625b278c01491074
George Goldberg <george@grundleborg.com>
2015-08-12 16:43:14 +0100

    Test TrainStatus LateReason.

--- tests/test_train_status_message.py
+++ tests/test_train_status_message.py
@@ -32,4 +32,21 @@ class TestTrainStatusMessage:
         assert(1 == len(m.locations))
         assert(False is m.reverse_formation)
 
- 
+    
+    def test_train_status_message_late_reason_tiploc(self):
+        m = self.get_train_status_message_from_file("tests/data/train_status_message__late_reason_tiploc.xml")
+        assert(None is not m)
+
+        assert("201508123594283" == m.rid)
+        assert("L06321" == m.uid)
+        assert(datetime.date(2015, 8, 12) == m.start_date)
+        assert(1 == len(m.locations))
+        assert(False is m.reverse_formation)
+
+        l = m.late_reason
+        assert(None is not l)
+        assert(153 == l.code)
+        assert("CRYSTLP" == l.tiploc)
+        assert(False is l.near)
+
+
